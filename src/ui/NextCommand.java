0fae9e272d2d0754f0a9ffe168fb84e1d55e1c10
hunte <hunterofseries13@gmail.com>
2024-01-20 20:09:48 +0000

    Create class NextCommand

---
+++ src/ui/NextCommand.java
@@ -0,0 +1,18 @@
+package ui;
+
+import ui.ImageDisplay;
+
+public class NextCommand implements Command {
+
+    private final ImageDisplay display;
+
+    public NextCommand(ImageDisplay display) {
+        this.display = display;
+    }
+
+    @Override
+    public void execute() {
+        display.show(display.getCurrentImage().next());
+    }
+
+}
